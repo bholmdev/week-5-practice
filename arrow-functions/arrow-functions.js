6501aa50ec82dbdbd031a8c27fd410e18335b48f
Brandon <112407387+bholmdev@users.noreply.github.com>
2024-08-27 12:08:30 -0500

    arrow functions

---
+++ arrow-functions/arrow-functions.js
@@ -0,0 +1,11 @@
+const adder = (a,b) => a + b;
+console.log(adder(2,3));
+
+const adder2 = (a,b) => {
+    const c = a + b;
+    return c;
+}
+console.log(adder2(2,3));
+
+const double = a => a * 2;
+console.log(double(12));
